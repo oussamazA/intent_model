76083daa37dc77913af88e243b167ed66d19206f
oussamazeghmouri <106214134+oussamazA@users.noreply.github.com>
2025-05-13 23:22:14 +0100

    app.py

--- app.py
+++ app.py
@@ -32,6 +32,6 @@ def respond():
 
     return jsonify({'reply': reply_text})
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
